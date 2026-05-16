f825492f6d1fbd790bf24c342dc650a22c0c9640
Calvinmuemah <kelvinmuemah855@gmail.com>
2026-05-16 18:45:47 +0300

    modify pwa conf

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,16 +7,29 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
     VitePWA({
       registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+      ],
+
       manifest: {
         name: "MMUST Mental Wellness",
         short_name: "MMUSTCare",
         description: "Digital CBT Mental Wellness Platform",
+
         theme_color: "#0f172a",
         background_color: "#ffffff",
+
         display: "standalone",
+        orientation: "portrait",
+        scope: "/",
         start_url: "/",
+
         icons: [
           {
             src: "/icon-192.png",
@@ -28,8 +41,49 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
       },
     }),
   ],
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import { VitePWA } from "vite-plugin-pwa";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       manifest: {
+//         name: "MMUST Mental Wellness",
+//         short_name: "MMUSTCare",
+//         description: "Digital CBT Mental Wellness Platform",
+//         theme_color: "#0f172a",
+//         background_color: "#ffffff",
+//         display: "standalone",
+//         start_url: "/",
+//         icons: [
+//           {
+//             src: "/icon-192.png",
+//             sizes: "192x192",
+//             type: "image/png",
+//           },
+//           {
+//             src: "/icon-512.png",
+//             sizes: "512x512",
+//             type: "image/png",
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+// });
